a0191cdef15d18334e5a03ec9b5235634c4c218e
Vaniluthra <vaniluthra05@gmail.com>
2024-04-04 21:38:59 +0530

    Create Training_MNIST_Dataset.py

---
+++ Training_MNIST_Dataset.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+train_images = train_images.reshape((60000, 28, 28, 1)).astype('float32') / 255
+test_images = test_images.reshape((10000, 28, 28, 1)).astype('float32') / 255
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=5, batch_size=64, validation_split=0.2)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(f'Test accuracy: {test_acc}')
+
+predictions = model.predict(test_images[:20])
+
+fig, axes = plt.subplots(4, 5, figsize=(15, 10))  # 4 rows, 5 columns
+for i, ax in enumerate(axes.flat):
+    ax.imshow(test_images[i].reshape(28, 28), cmap='gray')
+    ax.set_title(f'Predicted: {tf.argmax(predictions[i])}, Actual: {tf.argmax(test_labels[i])}')
+    ax.axis('off')  # Turn off axis for cleaner display
+plt.tight_layout()  # Adjust layout for better spacing
+plt.show()
